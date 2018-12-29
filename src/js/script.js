8689c982485fd769e2930a71bb9ad818d07c55fb
friederjanmoerman <hijbrid@gmail.com>
2018-12-29 22:01:00 +0100

    gallery images have full view functionality on click

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,15 @@ var toggleNav = document.querySelector('.js-menu-toggle');
 toggleNav.onclick = function() {
   document.querySelector('.body').classList.toggle('open-menu');
 };
+
+var toggleFullview = document.getElementsByClassName('js-full-view');
+for (var i=0; i < toggleFullview.length; i++) {
+  toggleFullview[i].onclick = function() {
+    var fullViewOverlay = document.getElementById(this.getAttribute('data-number'));
+    var lazyLoadImg = fullViewOverlay.querySelector('.js-lazy-load');
+    fullViewOverlay.classList.add('active');
+    lazyLoadImg.style.backgroundImage = "url('" + lazyLoadImg.getAttribute('data-src') + "')";
+    console.log(fullViewOverlay);
+    console.log(lazyLoadImg);
+  };
+};
