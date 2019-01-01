3c9fb8972f234b4432e912fb09a2f4de112ff18d
friederjanmoerman <hijbrid@gmail.com>
2019-01-01 17:27:15 +0100

    finished the gallery by adding a back button and fixing a flex layout issue in Chrome

--- src/js/script.js
+++ src/js/script.js
@@ -8,9 +8,13 @@ for (var i=0; i < toggleFullview.length; i++) {
   toggleFullview[i].onclick = function() {
     var fullViewOverlay = document.getElementById(this.getAttribute('data-number'));
     var lazyLoadImg = fullViewOverlay.querySelector('.js-lazy-load');
+    var fullViewBackButton = fullViewOverlay.querySelector('.js-back');
     fullViewOverlay.classList.add('active');
+    document.querySelector('.body').classList.add('overlay-active');
     lazyLoadImg.style.backgroundImage = "url('" + lazyLoadImg.getAttribute('data-src') + "')";
-    console.log(fullViewOverlay);
-    console.log(lazyLoadImg);
+    fullViewBackButton.onclick = function() {
+      fullViewOverlay.classList.remove('active');
+      document.querySelector('.body').classList.remove('overlay-active');
+    }
   };
 };
